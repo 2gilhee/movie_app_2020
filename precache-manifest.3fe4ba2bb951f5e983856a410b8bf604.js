bf89362308a4caca03a2db8f8e265ea877e9eb20
eAPT2020 <2gilhee@naver.com>
2020-07-23 11:46:10 +0900

    Updates

---
+++ precache-manifest.3fe4ba2bb951f5e983856a410b8bf604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704203746cd09635fda8f8ea92c21b19",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d5e66bc35d447dc98f52",
+    "url": "/movie_app_2020/static/css/main.aac6dccc.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e66bc35d447dc98f52",
+    "url": "/movie_app_2020/static/js/main.78914c36.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
